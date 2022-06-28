ac7b4a6e7e732027c49c4b2896d5b231053ab32f
Vadim <v.marenov@bk.ru>
2022-06-28 12:47:27 +0300

    Only Test

---
+++ src/test/java/TestCreditCalculator.java
@@ -0,0 +1,33 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TestCreditCalculator {
+    private final double creditValue = 100000;  //rubbles
+    private final double creditRate = 13;   //%
+    private final double creditTerm = 12; //months
+
+    CreditCalculator sut = new CreditCalculator();
+
+    @Test
+    void getPaymentPerMonthTest() {
+        double expected = 8931.727571;
+        double actual = sut.getPaymentPerMonth(creditValue, creditRate, creditTerm);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void getFullCreditCostTest() {
+        double expected = 107180.73085409947;
+        double actual = sut.getFullCreditCost(creditValue, creditRate, creditTerm);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void getLoanRepaymentTest() {
+        double expected = 7180.73085409947;
+        double actual = sut.getLoanRepayment(creditValue, creditRate, creditTerm);
+        Assertions.assertEquals(expected, actual);
+    }
+
+
+}
